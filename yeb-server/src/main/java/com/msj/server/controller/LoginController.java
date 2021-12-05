437a27825e0eaaca27df27faa6f949497f9e63b7
msj <2669159659@qq.com>
2021-12-05 17:14:24 +0800

    获取当前用户信息controller

--- yeb-server/src/main/java/com/msj/server/controller/LoginController.java
+++ yeb-server/src/main/java/com/msj/server/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.msj.server.controller;
 
+import com.msj.server.pojo.Admin;
 import com.msj.server.pojo.AdminLoginParam;
 import com.msj.server.pojo.RespBean;
 import com.msj.server.service.IAdminService;
@@ -7,8 +8,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * 用户登录实体
@@ -23,6 +27,19 @@ public class LoginController {
     @PostMapping("/login")
     public RespBean login(AdminLoginParam adminLoginParam, HttpRequest request) {
         return adminService.login(adminLoginParam.getUsername(), adminLoginParam.getPassword(), request);
+    }
+
+    @ApiOperation(value = "获取当前用户信息")
+    @GetMapping("/admin/info")
+    public Admin getAdminInfo(Principal principal) {
+        if(null == principal) {
+            return null;
+        }
+        String username = principal.getName();
+        Admin admin = adminService.getAdminByUserName(username);
+        // 为了防止密码泄漏，一般密码不返回
+        admin.setPassword(null);
+        return admin;
     }
 
     @ApiOperation(value = "退出登录")
