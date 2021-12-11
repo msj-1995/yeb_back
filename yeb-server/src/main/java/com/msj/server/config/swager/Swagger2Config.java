92927df3d94555f511f81a99acae50de09b50bb1
msj <2669159659@qq.com>
2021-12-11 22:50:47 +0800

    配置swaagger登录

--- yeb-server/src/main/java/com/msj/server/config/swager/Swagger2Config.java
+++ yeb-server/src/main/java/com/msj/server/config/swager/Swagger2Config.java
@@ -6,10 +6,13 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * Swagger2配置类
@@ -28,7 +31,9 @@ public class Swagger2Config {
                 .apis(RequestHandlerSelectors.basePackage("com.msj.server.controller"))
                 // 配置扫描任何路径
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securityContexts()
+                .securitySchemes();
     }
 
     private ApiInfo apiInfo() {
@@ -39,4 +44,6 @@ public class Swagger2Config {
                 .version("1.0")
                 .build();
     }
+
+    private List<ApiKey>
 }
