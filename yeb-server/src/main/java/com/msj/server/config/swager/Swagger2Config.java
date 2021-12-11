77f23b2c04a218f48b9c5b870ac0e97460ed05d7
msj <2669159659@qq.com>
2021-12-11 23:06:16 +0800

    默认认证

--- yeb-server/src/main/java/com/msj/server/config/swager/Swagger2Config.java
+++ yeb-server/src/main/java/com/msj/server/config/swager/Swagger2Config.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -66,5 +64,14 @@ public class Swagger2Config {
         return SecurityContext.builder(defaultAuth())
                 .forPaths(PathSelectors.regex(pathRegex))
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> result = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        result.add(new SecurityReference("Authorization", authorizationScopes));
+        return result;
     }
 }
