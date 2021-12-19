3a4f9c98613fb282296bdfe6d5c4f01913c07b56
msj <2669159659@qq.com>
2021-12-19 23:35:27 +0800

    修改

--- yeb-server/src/main/java/com/msj/server/mapper/AdminMapper.java
+++ yeb-server/src/main/java/com/msj/server/mapper/AdminMapper.java
@@ -2,9 +2,6 @@ package com.msj.server.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.msj.server.pojo.Admin;
-import com.msj.server.pojo.Menu;
-
-import java.util.List;
 
 /**
  * <p>
@@ -15,11 +12,4 @@ import java.util.List;
  * @since 2021-12-04
  */
 public interface AdminMapper extends BaseMapper<Admin> {
-    /**
-     * 通过用户id查询菜单列表
-     * 这里为了减少数据库的查询语句，我们把当前用户id对应的菜单和子菜单一次性查出来返回给前端
-     * 这里的用户和菜单列表是自关联关系
-     * 这里的用户和菜单列表是自关联关系
-     */
-    List<Menu> getMenuByAdminId(Integer id);
 }
--- yeb-server/src/main/java/com/msj/server/service/impl/MenuServiceImpl.java
+++ yeb-server/src/main/java/com/msj/server/service/impl/MenuServiceImpl.java
@@ -2,9 +2,11 @@ package com.msj.server.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.msj.server.mapper.MenuMapper;
+import com.msj.server.pojo.Admin;
 import com.msj.server.pojo.Menu;
 import com.msj.server.service.IMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,12 +23,13 @@ import java.util.List;
 public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IMenuService {
     @Autowired
     private MenuMapper menuMapper;
+
     /**
      * 根据用户id查询菜单列表
      * @return
      */
     @Override
     public List<Menu> getMenuByAdminId() {
-        return null;
+        return menuMapper.getMenuByAdminId(((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
     }
 }
